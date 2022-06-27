2579639b6f087e31120baae139aa25d50f49ff2f
Martin Moscovich <m.moscovich@configureid.com>
2022-06-27 17:56:53 -0300

    Fixed gha.mjs

---
+++ release_notes/gha.mjs
@@ -0,0 +1,9 @@
+import { github, core } from "../libs/gh.mjs";
+
+/** Uses the GitHub Actions Run Context */
+export function getContextAndGraphQL() {
+  const ghToken = core.getInput("GITHUB_TOKEN");
+  const octokit = github.getOctokit(ghToken);
+
+  return { context: github.context, graphql: octokit.graphql.bind(octokit) };
+}
